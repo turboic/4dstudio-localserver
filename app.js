d4c2c976edd036d42c2c96313d51fcb3be0a790c
Jerome Etienne <jerome.etienne@gmail.com>
2015-07-14 12:21:40 +0100

    added cors + cleanup

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// allow cors - see http://enable-cors.org/server_expressjs.html
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', require('./routes/index'));
 app.use('/edit', require('./routes/edit'));
 
--- public/nativefileedit-client.js
+++ public/nativefileedit-client.js
@@ -1,26 +1,20 @@
-var NativeFileEdit = function(content, basename, callback){
-	var url = 'edit/'
+var NativeFileEdit = function(localServerUrl, content, basename, callback){
+	var url = localServerUrl+'edit/'
 	url += '?basename='+encodeURI(basename);
 	url += '&content='+encodeURI(content);
 	// var url = 'edit/';
 
 	var request = new XMLHttpRequest();
 	request.addEventListener('load', function(){
-		// 		console.log(JSON.parse(this.responseText));
-		// console.dir(this)
-		// debugger;
-		// 
-
 		var responseJSON = JSON.parse(this.responseText);
 		if( responseJSON.status === 'ok' ){
 			callback(null, responseJSON.newContent)				
 		}else{
 			callback(responseJSON.status, responseJSON.newContent)
 		}
-		// var response
-		// callback(newContent)
 	})
 	// FIXME sync XMLHttpRequest
 	request.open("get", url);
+	
 	request.send();		
 }
